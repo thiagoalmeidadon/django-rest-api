93cf7c5b0d5d8932bb362850e14fa0b9a06f1b41
thiagoalmeidadon <thiagodons@gmail.com>
2021-06-25 00:19:52 -0300

    implementando urls

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from livros.api import viewsets as livroview
+
+
+router = routers.DefaultRouter()
+
+router.register(r'livros/', livroview.LivrosViewSet, basename='Livros')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
